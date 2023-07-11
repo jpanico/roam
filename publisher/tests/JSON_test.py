3abe9d613358542496f1c88fa707427d02f03576
jpanico <joe@panmachine.biz>
2023-07-11 07:35:57 -0400

    create publisher Python project

---
+++ publisher/tests/JSON_test.py
@@ -0,0 +1,35 @@
+import unittest
+import json
+
+from common.LoggingConfig import logging, configure_logging
+
+class JSON_test(unittest.TestCase):
+
+
+    def test_read(self):
+        jsonFile = open("./tests/data/Page 3.json")
+
+        # returns JSON object as 
+        # a dictionary
+        jsonObj: list[dict] = json.load(jsonFile)
+
+        logging.debug(f"jsonObj: {jsonObj}, len: {len(jsonObj)}")
+        self.assertEquals(len(jsonObj), 30)
+        self.assertEquals(
+            jsonObj[0], 
+            {'text': 'Page 3', 'children': ['IO75SriD8', 'uFL-CFN6i'], 'uid': 'hfm6NKq2c', 'vertex-type': 'roam/page', 'media-type': 'text/plain'} 
+        )
+        self.assertEquals(
+            jsonObj[29], 
+            {'text': 'Block 1.2', 'children': ['Wu-bKjjdJ'], 'uid': 'mvVww9zGd', 'vertex-type': 'roam/block', 'media-type': 'text/plain'} 
+        )
+
+
+    def setUp(self):
+        configure_logging()
+        logging.debug("logging configured")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- publisher/tests/Zip_test.py
+++ publisher/tests/Zip_test.py
@@ -1,5 +1,5 @@
 import unittest
-import logging
+import zipfile
 
 from common.LoggingConfig import logging, configure_logging
 
@@ -7,8 +7,14 @@ class Zip_test(unittest.TestCase):
 
 
     def test_read(self):
-        const = "world"
-        logging.info(f"hello: {const}")
+        zipFile = zipfile.ZipFile("./tests/data/random.zip", "r")
+        paths: list[str] = zipFile.namelist()
+        logging.debug(f"paths: {paths}")
+        self.assertEqual(paths, ['Page 3/', 'Page 3/files/', 'Page 3/hello.txt', 'Page 3/files/0bca50cb-655a-4cf2-adbe-ca77b402f863.pdf', 'Page 3/files/5211ec15-6748-4226-ad9f-fb358bfb40ac.pdf', 'Page 3/files/81770416-701c-4099-b585-ff0988894cc9_readme.md', 'Page 3/files/9b673aae-8089-4a91-84df-9dac152a7f94_flower.jpeg', 'Page 3/files/cb0679e6-5f80-4a86-bc24-240b3da7ae41.pdf'])
+
+        readmeText: str = zipFile.read(paths[5]).decode(encoding="utf-8")
+        logging.debug(f"readmeText: {readmeText}")
+        self.assertTrue(readmeText.startswith("## Freenove Ultimate Starter Kit for Raspberry Pi"))
 
 
     def setUp(self):
