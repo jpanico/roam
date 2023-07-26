79670a8f2d2ebddc5c9a2040d3b864fe54bdf300
jpanico <joe@panmachine.biz>
2023-07-26 17:03:46 -0400

    cleanup

--- publisher/common/log.py
+++ publisher/common/log.py
@@ -1,21 +1,21 @@
 import sys
-import logging
+from logging import getLogger, Logger, Formatter, StreamHandler, DEBUG
 
 _is_configured: bool = False
 
 
-def configure_logging(level: int = logging.DEBUG):
+def configure_logging(level: int = DEBUG) -> None: 
     global _is_configured
     if _is_configured:
         return
 
     # Reconfiguring the logger here will also affect test running in the PyCharm IDE (i.e. IntelliJ Python plugin)
-    logger = logging.getLogger()
+    logger: Logger = getLogger()
     logger.setLevel(level)
-    formatter = logging.Formatter('%(relativeCreated)d (%(name)s) | %(levelname)s | [%(module)s:%(lineno)s] | %(funcName)s | %(message)s')
-    ch = logging.StreamHandler(sys.stdout)
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
+    formatter: Formatter = Formatter('%(relativeCreated)d (%(name)s) | %(levelname)s | [%(module)s:%(lineno)s] | %(funcName)s | %(message)s')
+    console_handler: StreamHandler = StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
 
     _is_configured = True
 
